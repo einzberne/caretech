b2317dcc06f1773b70470ba43e73be7d71db540f
bento-scg <thamatit@scg.com>
2023-05-28 15:10:43 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/caretech/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.caretech
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'util.dart';
+import 'ForgetPassword.dart';
+
+void main() {
+  runApp(NewPassword());
+}
